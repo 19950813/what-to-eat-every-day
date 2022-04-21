d8cda38999bcac5be0d059f933afc8261d93848d
19950813 <957824560@qq.com>
2022-04-21 17:01:22 +0800

    随机菜单

--- xia_chu_fang.py
+++ xia_chu_fang.py
@@ -3,6 +3,7 @@ import time
 from bs4 import BeautifulSoup
 import random
 import json
+# 本周最受欢迎
 url_template = 'http://www.xiachufang.com/explore/?page={num}'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
